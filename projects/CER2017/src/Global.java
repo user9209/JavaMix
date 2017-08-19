ffed9930f1e80ad988610e55808da4aa0fc400a4
Georg Schmidt <gs-develop@gs-sys.de>
2017-08-19 15:48:06 +0200

    see times

--- projects/CER2017/src/Global.java
+++ projects/CER2017/src/Global.java
@@ -8,6 +8,7 @@ public class Global {
     public static final boolean defaultFunction = true;
 
     public static double function(double x, double t) {
+        System.out.println("t = " + t);
         return Math.cos(4d / 3 * t + 1d / 3) + 0.5 * x;
     }
 }
