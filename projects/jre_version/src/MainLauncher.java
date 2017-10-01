bf3664b88d296aa91ea9c71111d23b8bce270881
Georg Schmidt <gs-develop@gs-sys.de>
2017-10-02 00:31:25 +0200

    update

--- projects/jre_version/src/MainLauncher.java
+++ projects/jre_version/src/MainLauncher.java
@@ -6,14 +6,14 @@
  * Created on 10-2017.
  */
 
-public class MiniLoader {
+public class MainLauncher {
     public static void main(String... args) {
         try {
-            MiniLoader.class.getClassLoader().loadClass("javafx.application.Application");
+            MainLauncher.class.getClassLoader().loadClass("javafx.application.Application");
         } catch (Throwable  e) { // ClassNotFoundException and NoClassDefFoundError
             NoFXMessage.showMessageAndExit("No JavaFX found","Your computer does not provide JavaFX 8.");
             return;
         }
-        BootFX.startFX();
+        StartJavaFxApp.startFX();
     }
 }
--- projects/jre_version/src/StartJavaFxApp.java
+++ projects/jre_version/src/StartJavaFxApp.java
@@ -6,7 +6,7 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class BootFX extends Application {
+public class StartJavaFxApp extends Application {
 
     public static void startFX(){
         launch();
